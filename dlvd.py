46025e7d8d99a612ce6aafa21397c1151ef1ad2a
Richie <yc.richie@gmail.com>
2025-04-12 01:35:25 +0800

    pass human verification

--- dlvd.py
+++ dlvd.py
@@ -9,6 +9,7 @@ ydl_opts = {
     'format': 'bestvideo[height=1080]+bestaudio/best',  # 下載 1080p 影片 + 最佳音訊
     'merge_output_format': 'mp4',  # 合併後輸出 MP4 格式
     'outtmpl': 'videos/%(title)s.%(ext)s',  # 指定儲存路徑
+    'cookiefile': 'cookies.txt',  # ← 加入這一行！
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
