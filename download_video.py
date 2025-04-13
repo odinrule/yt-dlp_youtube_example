3ed2050dd7fe6a7d64e3d510c0f7ed2319d8f9fa
Richie <yc.richie@gmail.com>
2025-04-14 01:35:25 +0800

    adding comment and change input fucntion

--- download_video.py
+++ download_video.py
@@ -1,12 +1,12 @@
 import yt_dlp
 
-url = input("請輸入 YouTube 影片網址：")
+url = input("請輸入影片網址：")
 
 ydl_opts = {
     'format': 'bestvideo[height=1080]+bestaudio/best',
     'merge_output_format': 'mp4',
     'outtmpl': 'videos/%(title)s.%(ext)s',
-    'cookiesfrombrowser': ('chrome',),
+    'cookiesfrombrowser': ('chrome',),  # 自動從chrome瀏覽器讀取cookies
     'geo_bypass': True,
     'force_ipv4': True,
 }
