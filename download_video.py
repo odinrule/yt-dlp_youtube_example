aef175b86e83bda2a8a3b58149891a51d6110eba
Richie <yc.richie@gmail.com>
2025-04-14 00:32:15 +0800

    cookies verify automatically

--- download_video.py
+++ download_video.py
@@ -1,12 +1,14 @@
 import yt_dlp
 
-url = 'https://youtu.be/wpyq6ba6W9E?feature=shared'
+url = input("請輸入 YouTube 影片網址：")
 
 ydl_opts = {
     'format': 'bestvideo[height=1080]+bestaudio/best',
     'merge_output_format': 'mp4',
     'outtmpl': 'videos/%(title)s.%(ext)s',
-    'cookiefile': 'cookies.txt',  # ← 加入這一行！
+    'cookiesfrombrowser': ('chrome',),
+    'geo_bypass': True,
+    'force_ipv4': True,
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
